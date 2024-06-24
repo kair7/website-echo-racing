ae87fee6f006ea8db468acbf73d2a3a084512ecc
Kai <kai.schlumm@tms-badoldesloe.de>
2024-06-24 22:11:22 +0200

    added a page reveal animation

--- main.js
+++ main.js
@@ -15,6 +15,68 @@ window.onload = function() {
     });
 }
 
+let tl = gsap.timeline({delay: 0});
+
+tl.to(".col", {
+    top: 0,
+    duration: 3,
+    ease: "power4.inOut"
+})
+
+tl.to(".c-1 .item", {
+    top: 0,
+    stagger: 0.25,
+    duration: 3,
+    ease: "power4.inOut"
+}, "-=2")
+
+tl.to(".c-2 .item", {
+    top: 0,
+    stagger: -0.25,
+    duration: 3,
+    ease: "power4.inOut"
+}, "-=4")
+
+tl.to(".c-3 .item", {
+    top: 0,
+    stagger: 0.25,
+    duration: 3,
+    ease: "power4.inOut"
+}, "-=4")
+
+tl.to(".c-4 .item", {
+    top: 0,
+    stagger: -0.25,
+    duration: 3,
+    ease: "power4.inOut"
+}, "-=4")
+
+tl.to(".c-5 .item", {
+    top: 0,
+    stagger: 0.25,
+    duration: 3,
+    ease: "power4.inOut"
+}, "-=4")
+
+tl.to(".container", {
+    scale: 6,
+    duration: 4,
+    ease: "power4.inOut"
+}, "-=2")
+
+tl.to(".nav-item a, .title p, .slide-num p, .preview img", {
+    top: 0,
+    stagger: 0.075,
+    duration: 3,
+    ease: "power3.out"
+}, "-=1.5")
+
+tl.to(".icon ion-icon, .icon ion-icon", {
+    scale: 1,
+    stagger: 0.05,
+    ease: "power3.out",
+}, "-=1")
+
 gsap.registerPlugin(ScrollTrigger);
 window.addEventListener("load", function () {
     const slides = gsap.utils.toArray(".slide");
